cf41d371634fa1b5c0f5ad3d76d32660d8c46f14
gaurav-vibhandik <gaurav.vibhandik@techbulls.com>
2023-10-26 16:12:56 +0530

    --working backend_dev branch:insert,update,delete order,OL,ol_topping added with full scope

--- src/main/java/com/myapp/model/Order.java
+++ src/main/java/com/myapp/model/Order.java
@@ -25,7 +25,7 @@ public class Order {
     private String status ;
     @NotNull(message = "non-zero amount must be mentioned")
     private int totalAmount;
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime orderDateTime;
     @NotNull(message = "delivery address must be mentioned")
     private String deliveryAddress;
